32a9c1ac2fbe784d8c8d79f4b72e2d5844974482
rhinocerose <ashar.k.latif@gmail.com>
2020-05-03 02:06:52 -0400

    quick commit

---
+++ bin/pandoc/panbuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+name=`echo $1 | cut -f1 -d'.'`
+style=zenburn
+engine=xelatex
+template="/home/asharkl/.dotfiles/bin/pandoc/default-md.tex"
+#
+pandoc -H "/home/asharkl/.dotfiles/bin/pandoc/pandoc-md.tex" "/home/asharkl/.dotfiles/bin/pandoc/pandoc-md.yaml" -o $name.$2  $1.md	\
+	--pdf-engine="$engine" 		\
+	--highlight-style="$style" 		\
+	--template="$template"			\
+	--listings						\
+	--variable subparagraph			\
