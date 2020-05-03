9d3c652ccfc750dc2d22dd09502886d8193a6ba4
rhinocerose <ashar.k.latif@gmail.com>
2020-05-02 22:30:59 -0400

    think I'm done

--- zsh/path.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-[[ ":$PATH:" != *":/bin:"* ]] && PATH="/bin:${PATH}"
-[[ ":$PATH:" != *":~/.local/bin:"* ]] && PATH="~/.local/bin:${PATH}"
-[[ ":$PATH:" != *":/usr/bin:"* ]] && PATH="/usr/bin:${PATH}"
-[[ ":$PATH:" != *":/usr/sbin:"* ]] && PATH="/usr/sbin:${PATH}"
-[[ ":$PATH:" != *":$HOME/bin:"* ]] && PATH="$HOME/bin:${PATH}"
-[[ ":$PATH:" != *":$DOTFILES_DIR/bin:"* ]] && PATH="$DOTFILES_DIR/bin:${PATH}"
-[[ ":$PATH:" != *":~/.npm-global/bin:"* ]] && PATH="~/.npm-global/bin:${PATH}"
