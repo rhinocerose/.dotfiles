90ef9a16ec80471d45d298f17d00abf4290e83b9
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2020-06-01 17:33:51 -0400

    Create appearance.sh

---
+++ appearance.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+just-colors --theme loudpastel --no-apply
+cat $HOME/.config/i3/config.base \
+    $HOME/.config/just-colors/cache/colors.local > $HOME/.dotfiles/.config/i3/config
+
+ln -sfn ~/.config/just-colors/cache/colors.conf ~/.config/kitty/colors.conf	
+ln -sfn ~/.config/just-colors/cache/colors.Xresources ~/colors.Xresources
+ln -sfn ~/.config/just-colors/cache/config.py ~/.config/qutebrowser/config.py
+ln -sfn ~/.config/just-colors/cache/zathurarc ~/.config/zathura/zathurarc
+ln -sfn ~/.dotfiles/.config/i3/config ~/.config/i3/config
