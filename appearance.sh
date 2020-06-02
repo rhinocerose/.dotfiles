98ccdb2979e50a52e50d05d13cca8146eb2bb6c7
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2020-06-01 23:21:35 -0400

    Update appearance.sh

--- appearance.sh
+++ appearance.sh
@@ -5,6 +5,10 @@ cat $HOME/.dotfiles/.config/i3/config.base $HOME/.config/just-colors/cache/color
 
 ln -sfn ~/.config/just-colors/cache/colors.conf ~/.config/kitty/colors.conf	
 ln -sfn ~/.config/just-colors/cache/colors.Xresources ~/colors.Xresources
+rm -rf ~/.config/qutebrowser
+mkdir ~/.config/qutebrowser
 ln -sfn ~/.config/just-colors/cache/config.py ~/.config/qutebrowser/config.py
+rm -rf ~/.config/zathura
+mkdir ~/.config/zathura
 ln -sfn ~/.config/just-colors/cache/zathurarc ~/.config/zathura/zathurarc
 ln -sfn ~/.dotfiles/.config/i3/config ~/.config/i3/config
