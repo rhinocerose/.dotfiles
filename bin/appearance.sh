720f62ced68f67d25d3a4532debe0daa4a68ff32
rhinocerose <ashar.k.latif@gmail.com>
2020-06-02 02:22:39 -0400

    quick commit

--- bin/appearance.sh
+++ bin/appearance.sh
@@ -7,7 +7,6 @@ ln -sfn ~/.config/just-colors/cache/colors.conf ~/.config/kitty/colors.conf
 ln -sfn ~/.config/just-colors/cache/colors.Xresources ~/colors.Xresources
 mkdir -pv ~/.config/qutebrowser
 ln -sfn ~/.config/just-colors/cache/config.py ~/.config/qutebrowser/config.py
-m -rf ~/.config/zathura
 mkdir -pv ~/.config/zathura
 ln -sfn ~/.config/just-colors/cache/zathurarc ~/.config/zathura/zathurarc
 ln -sfn ~/.dotfiles/.config/i3/config ~/.config/i3/config
--- display.sh
+++
@@ -1,2 +0,0 @@
-xrandr --output VGA1 --mode 1920x1080
-xrandr --output LVDS1 --off
