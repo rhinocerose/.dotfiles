f0cc438afaf46621622b6ac00403c5707aeeddf9
rhinocerose <ashar.k.latif@gmail.com>
2020-06-18 23:11:29 -0400

    quick commit

--- bin/appearance.sh
+++ bin/appearance.sh
@@ -2,9 +2,11 @@
 
 vimplugpath=~/.vim/autoload/plugged
 nvimplugpath=~/.config/nvim/autoload/plugged
+
 just-colors --theme "$1" --no-apply
+
 cat "$HOME"/.dotfiles/.config/i3/config.base "$HOME"/.config/just-colors/cache/i3-colors.local > "$HOME"/.dotfiles/.config/i3/config
-cat "$HOME"/.dotfiles/.config/qutebrowser/config.py.local "$HOME"/.config/just-colors/cache/qutebrowser-config.py > "$HOME"/.dotfiles/.config/qutebrowser/config.py
+cat "$HOME"/.dotfiles/.config/qutebrowser/base-config.py "$HOME"/.config/just-colors/cache/qutebrowser-config.py > "$HOME"/.dotfiles/.config/qutebrowser/config.py
 
 ln -sfn ~/.config/just-colors/cache/kitty-colors.conf ~/.config/kitty/colors.conf	
 ln -sfn ~/.config/just-colors/cache/colors.Xresources ~/colors.Xresources
@@ -18,7 +20,7 @@ mkdir -pv ~/.dotfiles/.config/vimthemes/"$1"/colors
 
 mv ~/.config/just-colors/cache/colors.vim ~/.dotfiles/.config/vimthemes/"$1"/colors/"$1".vim
 ln -sfn ~/.dotfiles/.config/vimthemes/"$1"/colors/"$1".vim ~/.vim/colors/base16-"$1".vim
-ln -sfn ~/.dotfiles/.config/vimthemes/"$1"/colors/"$1".vim ~/.config/nvim/colors/base16-"$1".vim
+#ln -sfn ~/.dotfiles/.config/vimthemes/"$1"/colors/"$1".vim ~/.config/nvim/colors/base16-"$1".vim
 
 mv ~/.config/just-colors/cache/colors.airline ~/.dotfiles/.config/vimthemes/"$1"/autoload/airline/themes/"$1".vim
 ln -sfn ~/.dotfiles/.config/vimthemes/"$1"/autoload/airline/themes/"$1".vim $vimplugpath/vim-airline-themes/autoload/airline/themes/"$1".vim 
