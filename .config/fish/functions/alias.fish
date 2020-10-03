22aa5cbef1ca12a760eb8d9ed152ebd39e9322d9
rhinocerose <ashar.k.latif@gmail.com>
2020-10-03 16:31:44 -0400

    quick commit

--- .config/fish/functions/alias.fish
+++ .config/fish/functions/alias.fish
@@ -1,9 +1,13 @@
+function newsketch
+    arduino-cli sketch new "$argv"
+end
+
 function nanobuild
     arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328 "$argv"
 end
 
 function nanoflash
-    sudo avrdude -c avrisp2 -p m328p -U flash:w:"$argv"
+    sudo avrdude -c avrisp2 -p m328p -U flash:w:build/arduino.avr.nano/"$argv"
 end
 
 function condinstall
