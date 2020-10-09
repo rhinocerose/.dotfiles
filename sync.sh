f275a4002da710b420f9dfaca503a8536d51c954
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2020-10-09 17:57:53 -0400

    Delete sync.sh

--- sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s "remote:.hardware-dev" "$HOME/Drive/.hardware-dev"
-rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s "remote:.school-docs" "$HOME/Drive/.school-docs"
-rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s "remote:.synth-schema" "$HOME/Drive/.synth-schema"
