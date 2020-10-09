181ae9322fe9c09e84adde5b550dbfbb6731cf0d
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2020-10-09 18:09:18 -0400

    Delete path.zsh

--- .zsh/path.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-[[ ":$PATH:" != *":/bin:"* ]] && PATH="/bin:${PATH}"
-[[ ":$PATH:" != *":$HOME/.local/bin:"* ]] && PATH="$HOME/.local/bin:${PATH}"
-[[ ":$PATH:" != *":/usr/bin:"* ]] && PATH="/usr/bin:${PATH}"
-[[ ":$PATH:" != *":/usr/sbin:"* ]] && PATH="/usr/sbin:${PATH}"
-[[ ":$PATH:" != *":$HOME/bin:"* ]] && PATH="$HOME/bin:${PATH}"
-[[ ":$PATH:" != *":$DOTFILES_DIR/bin:"* ]] && PATH="$DOTFILES_DIR/bin:${PATH}"
-[[ ":$PATH:" != *":$HOME/.npm-global/bin:"* ]] && PATH="$HOME/.npm-global/bin:${PATH}"
