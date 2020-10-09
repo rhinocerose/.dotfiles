34a6dff2c4d9cc7ed59040f50204676539b9a629
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2020-10-09 18:00:52 -0400

    Update link.sh

--- install/link.sh
+++ install/link.sh
@@ -19,8 +19,6 @@
 	rm -rf ~/.config/neofetch
 	ln -sfn ~/.dotfiles/.config/neofetch ~/.config/neofetch
 	ln -sfn ~/.dotfiles/.config/i3status ~/.config/i3status
-    ln -sfn ~/dotfiles-private/.config/tizonia ~/.config/tizonia
-    ln -sfn ~/dotfiles-private/.config/mps-youtube ~/.config/mps-youtube
 	ln -sfn ~/.dotfiles/.config/starship.toml ~/.config/starship.toml
 	mkdir -pv ~/.ssh
 	ln -sfn ~/dotfiles-private/.config/ssh/config ~/.ssh/
