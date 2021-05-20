6087724f6f9cf68abcb76d73e27d1ca8e0ece78e
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2021-05-20 09:40:12 -0400

    Update botclean.sh

--- bin/botclean.sh
+++ bin/botclean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# Clean up cruft (subtitles, nfo files, etc) left behind by botcleann
+# Usually invoked by rename.sh
 path=$1
 filebot -script fn:cleaner $path
 tempfile=$(find $path -type f -name '*.txt')
