a71c512d4b116ca473192cd79c2d5a7eea44730b
rhinocerose <ashar.k.latif@gmail.com>
2021-05-27 20:23:35 -0400

    Polystock cleanup

--- .config/polybar/polystock.py
+++ .config/polybar/polystock.py
@@ -5,38 +5,32 @@ Polystock
 Author: Ashar Latif
 Date: 2021-05-19
 Description: A ticker displayer for polybar.
-Displays the days highest gainer, biggest loser,
-top crypto or any custom stock ticker.
 Contact: ashar.k.latif@gmail.com
 """
 
-from yahoo_fin import stock_info as si
 from datetime import datetime
 from datetime import time
 import argparse
 
+from yahoo_fin import stock_info as si
+
 import symbols
 
-debug = False
+DEBUG = 0
 
 # How many decimal place to show in stock price.
-roundNumber = 2
+ROUND_NUMBER = 2
 
-# Market Status Indicators
-PREMARKET = 1
-POSTMARKET = 3
-MARKET_OPEN = 2
-MARKET_CLOSED = 0
-
-# Color Indicators
-RED = '\033[31m]'
-GREEN = '\033[32m]'
+# Color definitions
+TICKER_NAME_COLOR = '#fa83aa'
+POSITIVE_PERCENTAGE_COLOR = '#05fc15'
+NEGATIVE_PERCENTAGE_COLOR = '#fc0511'
 
 # Market Times
-premarket_open = time(4,0)
-market_open = time(9,30)
-market_close = time(16,0)
-postmarket_close = time(20,0)
+PREMARKET_OPEN = time(4,0)
+MARKET_OPEN = time(9,30)
+MARKET_CLOSE = time(16,0)
+POSTMARKET_CLOSE = time(20,0)
 
 
 def biggestloser():
@@ -44,7 +38,8 @@ def biggestloser():
     its stock price with format: 'TICKER': 'PRICE'."""
 
     day_losers = si.get_day_losers()
-    output = str(day_losers.at[0, 'Symbol']) + ': ' + str(round(si.get_live_price(day_losers.at[0, 'Symbol']), roundNumber))
+    output = str(day_losers.at[0, 'Symbol']) + ': ' + \
+             str(round(si.get_live_price(day_losers.at[0, 'Symbol']), ROUND_NUMBER))
     return output
 
 def biggestgainer():
@@ -52,7 +47,8 @@ def biggestgainer():
     its stock price with format: 'TICKER': 'PRICE'."""
 
     day_gainer = si.get_day_gainers()
-    output = str(day_gainer.at[0, 'Symbol']) + ': ' + str(round(si.get_live_price(day_gainer.at[0, 'Symbol']), roundNumber))
+    output = str(day_gainer.at[0, 'Symbol']) + ': ' + \
+             str(round(si.get_live_price(day_gainer.at[0, 'Symbol']), ROUND_NUMBER))
     return output
 
 def mostactive():
@@ -60,7 +56,8 @@ def mostactive():
     its stock price with format: 'TICKER': 'PRICE'."""
 
     day_active = si.get_day_most_active()
-    output = str(day_active.at[0, 'Symbol']) + ': ' + str(round(si.get_live_price(day_active.at[0, 'Symbol']), roundNumber))
+    output = str(day_active.at[0, 'Symbol']) + ': ' + \
+             str(round(si.get_live_price(day_active.at[0, 'Symbol']), ROUND_NUMBER))
     return output
 
 def topcrypto():
@@ -68,7 +65,8 @@ def topcrypto():
     with format: 'CRYPTO': 'PRICE'."""
 
     top_crypto = si.get_top_crypto()
-    output = str(top_crypto.at[0, 'Symbol']) + ': ' + str(round(si.get_live_price(top_crypto.at[0, 'Symbol']), roundNumber))
+    output = str(top_crypto.at[0, 'Symbol']) + ': ' + \
+             str(round(si.get_live_price(top_crypto.at[0, 'Symbol']), ROUND_NUMBER))
     return output
 
 def is_trading_hours():
@@ -77,80 +75,110 @@ def is_trading_hours():
     now = datetime.now().time()
     day = datetime.now().weekday()
     if day in range (0,5):
-        if ((now >= premarket_open) and (now < market_open)):
-            market_status = PREMARKET
-        elif ((now >= market_open) and (now < market_close)):
-            market_status = MARKET_OPEN
-        elif ((now >= market_close) and (now < postmarket_close)):
-            market_status = POSTMARKET
+        if MARKET_OPEN > now >= PREMARKET_OPEN:
+            market_status = "PREMARKET"
+        elif MARKET_CLOSE > now >= MARKET_OPEN:
+            market_status = "MARKET_OPEN"
+        elif POSTMARKET_CLOSE > now >= MARKET_CLOSE:
+            market_status = "POSTMARKET"
         else:
-            market_status = MARKET_CLOSED
+            market_status = "MARKET_CLOSED"
     else:
-        market_status = MARKET_CLOSED
+        market_status = "MARKET_CLOSED"
+    if DEBUG:
+        print(now)
+        print("Market status: " + str(market_status))
     return market_status
 
 def gain_loss(ticker, today):
+    """Returns: Colorized directional percentage movement of the ticker"""
     now = datetime.now()
     start = now.strftime("%m") + '/' + str(int(now.strftime("%d")) - 1) + '/' + now.strftime("%Y")
     yesterday = si.get_data(ticker, start_date = start).iloc[0]['close']
     percentage = round(((100 * today) / yesterday) - 100, 2)
-    if percentage > 0:
-        direction = '%{F#05fc15}▲%{F-}'
-        percentage = str('%{F#05fc15}' + str(percentage) + '%' + '%{F-}')
+    if DEBUG:
+        print("Percentage move:" + percentage)
+    if percentage >= 0:
+        color = POSITIVE_PERCENTAGE_COLOR
     elif percentage < 0:
-        direction = '%{F#fc0511}▼%{F-}'
-        percentage = str('%{F#fc0511}' + str(percentage) + '%' + '%{F-}')
-    return direction, percentage
+        color = NEGATIVE_PERCENTAGE_COLOR
+    return str('%{F' + color + '}' + str(percentage) + '%' + '%{F-}')
 
 def customticker(ticker):
     """Returns: stock price and ticker of a stock with format 'TICKER': 'PRICE'.
     Parameter: the ticker to get a stock price on and to display.
     Precondition: ticker is a string."""
-    now = datetime.now()
-    output = RED + ticker + ': ' + GREEN + str(round(tickerPriceToday, roundNumber)) + percentage + '%'
+    ticker_price = si.get_live_price(ticker)
+    percentage = gain_loss(ticker, ticker_price)
+    output = ticker + ': ' + str(round(ticker_price, ROUND_NUMBER)) + percentage
+    if DEBUG:
+        print(str(output))
     return output
 
 def ticker_parse(dictionary):
     """Returns: stock price and ticker of a stock with format 'TICKER': 'PRICE'.
     Parameter: the ticker to get a stock price on and to display.
     Precondition: ticker is a string."""
-    now = datetime.now()
     stocks = ""
+
+    market_status = is_trading_hours()
 
     for key in dictionary:
         if dictionary[key]["type"] != "stocks":
-            tickerPrice = si.get_live_price(dictionary[key]["ticker"])
+            ticker_price = si.get_live_price(dictionary[key]["ticker"])
         elif dictionary[key]["type"] == "stocks":
-            if is_trading_hours() == PREMARKET:
-                tickerPrice = si.get_premarket_price(dictionary[key]["ticker"])
-                market_status = "PRE"
-            elif is_trading_hours() == POSTMARKET:
-                tickerPrice = si.get_postmarket_price(dictionary[key]["ticker"])
-                market_status = "POST"
+            if market_status == "PREMARKET":
+                ticker_price = si.get_premarket_price(dictionary[key]["ticker"])
+            elif market_status == "POSTMARKET":
+                ticker_price = si.get_postmarket_price(dictionary[key]["ticker"])
+            elif market_status == "MARKET_OPEN":
+                ticker_price = si.get_live_price(dictionary[key]["ticker"])
             else:
-                tickerPrice = si.get_live_price(dictionary[key]["ticker"])
-                market_status = "OPEN"
-        direction, percentage = gain_loss(dictionary[key]["ticker"], tickerPrice)
-        key = str('%{F#fa83aa}' + key + '%{F-}')
-        output = key + ': '  + str(round(tickerPrice, roundNumber)) + ' ' + str(percentage) + ' | '
+                ticker_price = si.get_live_price(dictionary[key]["ticker"])
+        if DEBUG:
+            print(str(key) + str(ticker_price))
+        percentage = gain_loss(dictionary[key]["ticker"], ticker_price)
+        formatted_key = str('%{F' + TICKER_NAME_COLOR + '}' + key + '%{F-}')
+        output = formatted_key + ': '  + str(round(ticker_price, ROUND_NUMBER)) + \
+                 ' ' + str(percentage) + ' | '
+        if DEBUG:
+            print(output)
         stocks += output
+    if DEBUG:
+        print(stocks)
     stocks += market_status
     return stocks
 
-def addArguments():
+def parse_arguments():
     """Adds arguments from ArgParse and parses them to handle arguments"""
 
-    parser = argparse.ArgumentParser(description='Displays stock prices outputted in a simplified form for polybar.',
-                                     epilog='Output will always be in the format of: Biggest Loser, Biggest Gainer, Most Active, Top Crypto, Custom Ticker')
+    parser = argparse.ArgumentParser(description='Displays stock prices for polybar.',
+                                     epilog='Output will always be in the format of: \
+                                     Biggest Loser, Biggest Gainer, Most Active, \
+                                     Top Crypto, Custom Ticker')
 
     # add arguments to be called
-    parser.add_argument('--biggestloser', help='Prints the stock with the biggest drop in a given day.', action='store_true')
-    parser.add_argument('--biggestgainer', help='Prints the stock with the biggest gain in a given day.', action='store_true')
-    parser.add_argument('--mostactive', help='Prints the most active stock in a given day.', action='store_true')
-    parser.add_argument('--topcrypto', help='Prints the top cryptocurrency by market cap in a given day.', action='store_true')
-    parser.add_argument('--customticker', help='Display the price of a custom ticker.', type=str)
-    parser.add_argument('--mytickers', help='Display the price of my tickers.', action='store_true')
-    parser.add_argument('--getgroup', help='Display the price of major crypto assets.', action='store_true')
+    parser.add_argument('--biggestloser',
+                        help='Prints the stock with the biggest drop in a given day.',
+                        action='store_true')
+    parser.add_argument('--biggestgainer',
+                        help='Prints the stock with the biggest gain in a given day.',
+                        action='store_true')
+    parser.add_argument('--mostactive',
+                        help='Prints the most active stock in a given day.',
+                        action='store_true')
+    parser.add_argument('--topcrypto',
+                        help='Prints the top cryptocurrency by market cap in a given day.',
+                        action='store_true')
+    parser.add_argument('--customticker',
+                        help='Display the price of a custom ticker.',
+                        type=str)
+    parser.add_argument('--mytickers',
+                        help='Display the price of my tickers.',
+                        action='store_true')
+    parser.add_argument('--getgroup',
+                        help='Display the price of selected equties.',
+                        action='store_true')
 
     args = parser.parse_args()
 
@@ -182,4 +210,4 @@ def addArguments():
         print(stocks)
 
 if __name__ == '__main__':
-    addArguments()
+    parse_arguments()
