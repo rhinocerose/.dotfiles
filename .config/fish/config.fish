793f26a3ea4a309c76614532e7f831f967441e5d
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2021-08-05 15:30:10 -0400

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,4 @@
 source ~/.dotfiles/.config/fish/functions/*.fish
-#source ~/.dotfiles/.config/fish/functions/websearch
-
 
 set PATH "/bin:$PATH"
 set PATH "$HOME/.local/bin:$PATH"
@@ -13,7 +11,6 @@ set PATH "$HOME/.npm-global/bin:$PATH"
 set PATH "$HOME/npm-modules:$PATH"
 set PATH "/opt/gcc-arm/bin:$PATH"
 set PATH "$HOME/.gem/ruby/2.7.0/bin:$PATH"
-set PATH "$HOME/anaconda3/bin:$PATH"
 
 set -gx EDITOR vim
 set -gx VISUAL "$EDITOR"
@@ -31,9 +28,3 @@ set FZF_DEFAULT_OPTS "--color fg:242,bg:233,hl:65,fg+:15,bg+:234,hl+:108
 --bind alt-j:down,alt-k:up"
 
 starship init fish | source
-
-# # >>> conda initialize >>>
-# # !! Contents within this block are managed by 'conda init' !!
-# eval /home/arch/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# # <<< conda initialize <<<
-
