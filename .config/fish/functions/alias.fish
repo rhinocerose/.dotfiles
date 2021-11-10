251150398bda8c5a5979e77e6001395cb2b34412
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2021-11-10 09:12:34 -0500

    Update alias.fish

--- .config/fish/functions/alias.fish
+++ .config/fish/functions/alias.fish
@@ -317,18 +317,14 @@ end
 ##############################################################
 # MISC
 ##############################################################
-
-function condinstall
-    conda install -c conda-forge "$argv"
+function ytdl
+	youtube-dl --add-metadata --embed-thumbnail "$argv"
 end
 
 function gpgkey
     gpg --keyserver pool.sks-keyservers.net --recv-keys "$argv"
 end
 
-function cal
-	gcalcli --cal ashar.k.latif@gmail.com  quick
-end
 
 function pb
 	bash $HOME/.dotfiles/bin/pandoc/panbuild.sh $argv
